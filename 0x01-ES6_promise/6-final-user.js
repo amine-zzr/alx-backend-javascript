0d1e2badb72a4f0bdf0253093fe6ded882ad180d
amine-zzr <aminezoukri@gmail.com>
2024-10-06 12:12:56 +0100

    fixing style

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,19 +3,16 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      return results.map((result) => {
-        if (result.status === 'fulfilled') {
-          return {
-            status: result.status,
-            value: result.value,
-          };
-        } else {
-          return {
-            status: result.status,
-            value: result.reason.message,
-          };
-        }
-      });
-    });
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
+      return {
+        status: result.status,
+        value: result.reason.message,
+      };
+    }));
 }
