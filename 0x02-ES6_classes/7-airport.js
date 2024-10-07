383390f883eaaa46463fec467cf7965a67f5a96d
amine-zzr <aminezoukri@gmail.com>
2024-10-07 23:13:02 +0100

    the Airport class that customizes the toString() method to return the airport code

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,14 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string' || typeof code !== 'string') {
+      throw new TypeError('Name and code must be strings');
+    }
+
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
