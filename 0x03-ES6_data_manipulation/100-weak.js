ace7056038da20f65972fb1e1e84a62662b96d79
amine-zzr <aminezoukri@gmail.com>
2024-10-10 21:52:35 +0100

    implementing a function using WeakMap to track the number of calls to queryAPI for each endpoint

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  const queryCount = weakMap.get(endpoint);
+  if (queryCount >= 4) {
+    throw new Error('Endpoint load is high');
+  }
+  weakMap.set(endpoint, queryCount + 1);
+}
